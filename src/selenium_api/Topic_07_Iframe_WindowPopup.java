d7276cd5c082451a9fc849ac291449a2cd31111b
Phan Thi Bich Van <citvan@gmail.com>
2018-08-12 00:52:58 +0700

    Topic_05,06

---
+++ src/selenium_api/Topic_07_Iframe_WindowPopup.java
@@ -0,0 +1,39 @@
+package selenium_api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_07_Iframe_WindowPopup {
+    WebDriver driver;
+	
+    @BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.get("http://demo.guru99.com/v4/");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+    }
+    
+   
+	@Test
+	public void TC_01_Dropdownlist() {
+		
+		
+	}
+	@Test
+	public void TC_02_Textbox_Textarea() {
+		
+		
+	}
+		
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
