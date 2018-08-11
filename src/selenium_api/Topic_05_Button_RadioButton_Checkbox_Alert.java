cef3a85a91362614c45523aef6913be4b980f988
Phan Thi Bich Van <citvan@gmail.com>
2018-08-12 00:53:25 +0700

    Topic 05, 06

---
+++ src/selenium_api/Topic_05_Button_RadioButton_Checkbox_Alert.java
@@ -0,0 +1,133 @@
+package selenium_api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_05_Button_RadioButton_Checkbox_Alert {
+    WebDriver driver;
+	
+    @BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+    }
+    
+   
+	@Test (enabled = false)
+	public void TC_01_Button() {
+		driver.get("http://live.guru99.com/");
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//form[@id='login-form']")).isDisplayed());
+		Assert.assertEquals(driver.getCurrentUrl(), "http://live.guru99.com/index.php/customer/account/login/");
+		
+		//click button bang javascript
+		String button = "//a[@class='button']";
+		clickElementByJavascript(button);
+		Assert.assertTrue(driver.findElement(By.xpath("//form[@id='form-validate']")).isDisplayed());
+		Assert.assertEquals(driver.getCurrentUrl(), "http://live.guru99.com/index.php/customer/account/create/");
+	}
+	
+	
+	@Test (enabled = false)
+	public void TC_02_Checkbox() {
+	//Step 01 - Truy cap vao trang: http://demos.telerik.com/kendo-ui/styling/checkboxes
+		driver.get("http://demos.telerik.com/kendo-ui/styling/checkboxes");
+	//Step 02 + 03 - Click vao checkbox: Dual-zone air conditioning va kiem tra checkbox duoc chon
+
+		//Case 4: Dung 1 elemennt vua click duoc vua verify duoc
+		String dualZoneCheckbox = "//label[text()='Dual-zone air conditioning']/preceding-sibling::input";
+		clickElementByJavascript(dualZoneCheckbox);
+		Assert.assertTrue(isElementSelected(dualZoneCheckbox));
+	// Step 04 - Sau khi checkbox da duoc chon - deselect no va kiem tra no chua duoc chon
+		unCheckTheCheckbox(dualZoneCheckbox);
+		Assert.assertFalse(isElementSelected(dualZoneCheckbox));
+	}
+	
+	
+	@Test
+	public void TC_03_RadioButton() {
+		driver.get("http://demos.telerik.com/kendo-ui/styling/radios");
+		String petrol2Radio = "//label[text()='2.0 Petrol, 147kW']/preceding-sibling::input";
+		String diesel16Radio = "//label[text()='1.6 Diesel, 77kW']/preceding-sibling::input";
+		
+		clickElementByJavascript(petrol2Radio);
+		Assert.assertTrue(isElementSelected(petrol2Radio));
+	//Step 3 - Click vao raido khac, kiem tra radio 2 petrol chua duoc chon va chon lai	
+		clickElementByJavascript(diesel16Radio);
+		Assert.assertFalse(isElementSelected(petrol2Radio));
+		clickElementByJavascript(petrol2Radio);
+		Assert.assertTrue(isElementSelected(petrol2Radio));
+	}
+	
+	
+	@Test (enabled = false)
+	public void TC_04_Alert() {
+		driver.get("http://daominhdam.890m.com/");
+		driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();
+		Alert alert = driver.switchTo().alert();
+		String alertMsg = alert.getText();
+		Assert.assertTrue(alertMsg.equals("I am a JS Alert"));
+		alert.accept();
+		Assert.assertEquals(driver.findElement(By.xpath("//p[@id='result']")).getText(),"You clicked an alert successfully");
+	}
+	
+	
+	@Test (enabled = false)
+	public void TC_05_Confirm() {
+		driver.get("http://daominhdam.890m.com/");
+		driver.findElement(By.xpath("//button[text()='Click for JS Confirm']")).click();
+		Alert alert = driver.switchTo().alert();
+		String alertMsg = alert.getText();
+		Assert.assertTrue(alertMsg.equals("I am a JS Confirm"));
+		alert.dismiss();
+		Assert.assertEquals(driver.findElement(By.xpath("//p[@id='result']")).getText(),"You clicked: Cancel");
+	}
+	
+	
+	@Test (enabled = false)
+	public void TC_06_Prompt() {
+		driver.get("http://daominhdam.890m.com/");
+		driver.findElement(By.xpath("//button[text()='Click for JS Prompt']")).click();
+		Alert alert = driver.switchTo().alert();
+		String alertMsg = alert.getText();
+		Assert.assertTrue(alertMsg.equals("I am a JS prompt"));
+		alert.sendKeys("Automatic Testing online 05");
+		alert.accept();
+		Assert.assertEquals(driver.findElement(By.xpath("//p[@id='result']")).getText(),"You entered: Automatic Testing online 05");
+	}
+	
+	
+	public void clickElementByJavascript(String locator) {
+		WebElement element = driver.findElement(By.xpath(locator));
+	    JavascriptExecutor je = (JavascriptExecutor) driver;
+	    je.executeScript("arguments[0].click();", element);
+	}
+	
+	public boolean isElementSelected (String locator) {
+		WebElement element = driver.findElement(By.xpath(locator));
+		return element.isSelected();
+	}
+	
+	public void unCheckTheCheckbox (String locator) {
+		if (isElementSelected(locator)) {
+			clickElementByJavascript(locator);
+		}
+	}
+
+	
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
---
+++ src/selenium_api/Topic_06_UserInteractions.java
@@ -0,0 +1,141 @@
+package selenium_api;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_06_UserInteractions {
+    WebDriver driver;
+	
+    @BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+    }
+    
+   
+	@Test (enabled = false)
+	public void TC_01_HoverMouse() {
+		//Case 01:
+		driver.get("http://daominhdam.890m.com/");
+		WebElement hoverText = driver.findElement(By.xpath("//a[text()='Hover over me']"));
+		Actions action = new Actions(driver);
+		//hover mouse
+		action.moveToElement(hoverText).perform();
+		//Verify
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class='tooltip-inner']")).getText(), "Hooray!");
+		
+				
+		//Case 02:
+		driver.get("http://www.myntra.com/");
+		WebElement hoverMenu = driver.findElement(By.xpath("//span[@class='myntraweb-sprite desktop-iconUser sprites-user']"));
+		action = new Actions(driver);
+		action.moveToElement(hoverMenu).perform();
+		driver.findElement(By.xpath("//a[@data-track='login']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@class='login-box']")).isDisplayed());
+	}
+	
+	@Test (enabled = false)
+	public void TC_02_ClickAndHold() {
+		driver.get("http://jqueryui.com/resources/demos/selectable/display-grid.html");
+		
+		List <WebElement> selectList = driver.findElements(By.xpath("//li[@class='ui-state-default ui-selectee']"));
+		Actions action = new Actions(driver);
+		/*
+		//Case 1:
+		action.clickAndHold(selectList.get(0)).moveToElement(selectList.get(3)).release().perform();
+		List <WebElement> selected = driver.findElements(By.xpath("//li[@class='ui-state-default ui-selectee ui-selected']"));
+		Assert.assertEquals(selected.size(), 4);
+		*/
+		
+		//Case 2:
+		action.keyDown(Keys.CONTROL).build().perform();
+		selectList.get(0).click();
+		selectList.get(2).click();
+		selectList.get(4).click();
+		selectList.get(6).click();
+		selectList.get(8).click();
+		action.keyUp(Keys.CONTROL).build().perform();
+		List <WebElement> selected = driver.findElements(By.xpath("//li[@class='ui-state-default ui-selectee ui-selected']"));
+		Assert.assertEquals(selected.size(), 5);
+	}
+	
+	@Test (enabled = false)
+	public void TC_03_DoubleClick() {
+		driver.get("http://www.seleniumlearn.com/double-click");
+		WebElement doubleClick = driver.findElement(By.xpath("//button[text()='Double-Click Me!']"));
+		Actions action = new Actions(driver);
+		action.doubleClick(doubleClick).perform();
+		
+		Alert alert = driver.switchTo().alert();
+		Assert.assertEquals(alert.getText(), "The Button was double-clicked.");
+		alert.accept();
+	}
+	
+	@Test (enabled = false)
+	public void TC_04_RightClick() {
+		driver.get("http://swisnl.github.io/jQuery-contextMenu/demo.html");
+		WebElement rightClickButton = driver.findElement(By.xpath("//span[text()='right click me']"));
+		Actions action = new Actions(driver);
+		action.contextClick(rightClickButton).perform();
+		
+		//Hover vao quit
+		WebElement quitBefore = driver.findElement(By.xpath("//li[contains(@class,'context-menu-icon-quit')]"));
+		action.moveToElement(quitBefore).perform();
+		//Verify hover thanh congs
+		Assert.assertTrue(driver.findElement(By.xpath("//li[contains(@class,'context-menu-icon-quit') and contains(@class,'context-menu-hover') and contains(@class,'context-menu-visible')]")).isDisplayed());
+		action.click(quitBefore).perform();
+		Alert alert = driver.switchTo().alert();
+		Assert.assertEquals(alert.getText(), "clicked: quit");
+		alert.accept();
+		
+	}
+	
+	@Test (enabled = false)
+	public void TC_05_DragAndDrop_Case1() throws InterruptedException {
+		driver.get("http://demos.telerik.com/kendo-ui/dragdrop/angular");
+		WebElement sourceElement = driver.findElement(By.xpath("//div[@id='draggable']"));
+		WebElement targetElement = driver.findElement(By.xpath("//div[@id='droptarget']"));
+		Assert.assertEquals(targetElement.getText(), "Drag the small circle here.");
+		Actions action = new Actions(driver);
+		action.dragAndDrop(sourceElement, targetElement).perform();
+		Thread.sleep(4000);
+		Assert.assertEquals(targetElement.getText(), "You did great!");
+	}
+	
+	@Test 
+	public void TC_05_DragAndDrop_Case2() throws InterruptedException {
+		driver.get("http://jqueryui.com/resources/demos/droppable/default.html");
+		WebElement sourceElement = driver.findElement(By.xpath("//div[@id='draggable']"));
+		WebElement targetElement = driver.findElement(By.xpath("//div[@id='droppable']"));
+		Assert.assertEquals(targetElement.getText(), "Drop here");
+
+		Actions action = new Actions(driver);
+		action.dragAndDrop(sourceElement, targetElement).perform();
+		Thread.sleep(4000);
+		Assert.assertEquals(targetElement.getText(), "Dropped!");
+	}
+	
+	@Test (enabled = false)
+	public void TC_05_DragAndDrop_HTML5() throws InterruptedException {
+		
+	}
+	
+	
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
